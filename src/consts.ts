f54b9642a680c09b0b9b4095df5463fe2018db08
az-bnuy <antizio.bnuy@proton.me>
2026-05-06 23:43:18 +0200

    Change project titles and descriptions

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE_EN = 'Stop the ISS Spyware Expo in Prague';
+export const SITE_DESCRIPTION_EN = 'We are campaigning to stop the ISS Spyware Expo, an event that promotes and sells spyware technology, from taking place in Prague.';
+export const SITE_TITLE_CZ = 'Zastavte spyware veletrh ISS v Praze';
+export const SITE_DESCRIPTION_CZ = 'Kampaň proti uskutečnění spyware veletrhu ISS, kde se mají prodávat špionážní technologie.';
