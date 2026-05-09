34579ea7ea0446c40097b31471de742f0838d3b3
Lynx <notyourbussiness@haha.com>
2026-05-09 17:10:46 +0200

    Added page collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,35 @@
-import { defineCollection } from 'astro:content';
-import { glob } from 'astro/loaders';
-import { z } from 'astro/zod';
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: z.optional(image()),
-		}),
+  // Load Markdown and MDX files in the `src/content/companies/` directory.
+  loader: glob({ base: "./src/content/companies/cz", pattern: "*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      // pubDate: z.coerce.date(),
+      // updatedDate: z.coerce.date().optional(),
+      logo: z.optional(image()),
+    }),
 });
 
-export const collections = { blog };
+const blog_en = defineCollection({
+  // Load Markdown and MDX files in the `src/content/companies/` directory.
+  loader: glob({ base: "./src/content/companies/en", pattern: "*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      // pubDate: z.coerce.date(),
+      // updatedDate: z.coerce.date().optional(),
+      logo: z.optional(image()),
+    }),
+});
+
+export const collections = { blog, blog_en };
